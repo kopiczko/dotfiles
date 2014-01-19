eb813832e5559fd21fd1efb798ee04dabc283936
Paweł Kopiczko <kopiczko@gmail.com>
2014-01-19 18:07:13 +0100

    zsh installation script

---
+++ install-zsh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get isntall zsh
+
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
+chsh -s /bin/zsh  # in chef user resource shell attr
