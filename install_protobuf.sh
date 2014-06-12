d93070b0081e5db76a5d5f5cdabc6c659991e50d
Paweł Kopiczko <kopiczko@gmail.com>
2014-06-12 13:43:41 +0200

    protobuf installation script

---
+++ install_protobuf.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+VERSION=2.5.0
+TMPDIR="/tmp"
+
+file="protobuf-${VERSION}"
+url="http://protobuf.googlecode.com/files/${file}.tar.bz2"
+
+set -e
+
+if [ ! -e "$TMPDIR/${file}.tar.gz" ] ; then
+  curl -sS $url > "$TMPDIR/${file}.tar.bz2"
+fi
+if [ ! -d "$TMPDIR/$file" ] ; then
+  tar xfj "$TMPDIR/${file}.tar.bz2" -C $TMPDIR
+fi
+
+cd "$TMPDIR/$file"
+if [ ! -d "/opt/$file" ] ; then
+  ./configure --prefix="/opt/$file"
+  make
+  sudo make install
+fi
+
+cd /opt
+sudo ln -s $file protobuf
+
+sudo stow --no-folding -v -d /opt -t /usr/local -S protobuf
