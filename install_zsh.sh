1b0947500be11b9af1891c5f6961e8509c01a4f8
Paweł Kopiczko <kopiczko@gmail.com>
2014-09-09 09:59:16 +0200

    Improve ZSH installation script

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-sudo apt-get isntall zsh/$(lsb_release -cs)-backports
+set -e
 
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-ln -s $HOME/.dotfiles/zshrc .zshrc
+sudo apt-get install zsh/$(lsb_release -cs)-backports
+
+if [ ! -d $HOME/.oh-my-zsh ]; then
+  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+  ln -s $HOME/.dotfiles/zshrc .zshrc
+fi
 
 chsh -s /bin/zsh  # in chef user resource shell attr
