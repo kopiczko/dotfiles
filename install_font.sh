90a2d60e57427960c3286b768cecbd2f25dd52b1
Paweł Kopiczko <kopiczko@gmail.com>
2014-09-22 13:41:15 +0200

    Fix font installation script

--- install_font.sh
+++ install_font.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-fonts_dir="$HOME/.fonts"
+fonts_dir="$HOME/.local/share/fonts"
 
 # download font file
 dir="$fonts_dir/inconsolata-dz-for-powerline"
 file="$dir/Inconsolata-dz for Powerline.otf"
 url="https://github.com/Lokaltog/powerline-fonts/blob/master/InconsolataDz/Inconsolata-dz%20for%20Powerline.otf?raw=true"
-if [ ! -d $dir  ]; then
-  mkdir -p $dir
-  if [ ! -f $file ]; then
-    curl $url > $file
-  fi
+if [ ! -d "$dir" ]; then
+  mkdir -p "$dir"
+fi
+if [ ! -f "$file" ]; then
+  curl -sSL "$url" > "$file"
 fi
 
 # update font cache
-fc-cache -vf $fonts_dir
+fc-cache -vf "$fonts_dir"
