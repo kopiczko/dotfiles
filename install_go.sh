71f3beeed54676cebf42438c43698ed4a4a176d2
Paweł Kopiczko <kopiczko@gmail.com>
2018-01-02 17:07:33 +0100

    install_go: improve

--- install_go.sh
+++ install_go.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
+set os
+case $OSTYPE in
+    darwin*) os="darwin" ;;
+    linux*) os="linux" ;;
+    *) echo "ERROR unsuported OS $OSTYPE" >&2 ; exit 1 ;;
+esac
+
 version="1.9.2"
-os="darwin"
 arch="amd64"
 url="https://redirector.gvt1.com/edgedl/go/go${version}.${os}-${arch}.tar.gz"
-
-if [[ "$(uname)" != "Darwin" ]]; then
-    echo "ERROR currently works only on OSX" >&2
-    exit 1
-fi
 
 if [[ -z $GOROOT ]]; then
     echo "ERROR GOROOT env var must be set" >&2
@@ -34,10 +35,12 @@ mv /tmp/install_go/go $GOROOT
 
 rm -rf /tmp/install_go
 
+# Link k8s.io to github.com/kubernetes.
 mkdir -p $GOPATH/src
-rm -f $GOPATH/src/k8s.io
+rm -rf $GOPATH/src/k8s.io
+mkdir -p $GOPATH/src/github.com/kubernetes
 ln -s $GOPATH/src/github.com/kubernetes $GOPATH/src/k8s.io
 
 # Uninstall:
 #rm -rf $GOROOT/go
-#rm -f $GOPATH/src/k8s.io
+#rm -f $GOPATH/{bin,pkg,src}
