3af89721718dd08a2fe61609e0fb1149470a2a74
Paweł Kopiczko <kopiczko@gmail.com>
2018-01-04 09:18:53 +0100

    install: add tmux.conf; fix

--- install.sh
+++ install.sh
@@ -13,15 +13,15 @@ mk_link() {
 
     if [[ -h $t ]] && [[ "$(readlink $t)" == "$s" ]]; then
         echo "I $t exists and links to $s"
-        continue
+        return 0
     fi
     if [[ -h $t ]]; then
         echo "E $t exists but links to $(readlink $t), needs to be FIXED manually"
-        continue
+        return 1
     fi
     if [[ -e $t ]]; then
         echo "E $t exists but it is not a link, needs to be FIXED manually"
-        continue
+        return 1
     fi
     echo "I $t creating a link to $s"
     ln -s $s $t
@@ -42,6 +42,7 @@ files="$files envs"
 files="$files git_template"
 files="$files gitconfig"
 files="$files gitignore_global"
+files="$files tmux.conf"
 files="$files zshrc"
 
 for f in $files; do
