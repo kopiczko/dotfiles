9d69d280d3c3ce5e68988e526d429c0573615793
Paweł Kopiczko <kopiczko@gmail.com>
2018-03-06 07:46:38 +0100

    install_dep: update to v0.4.1

--- install_dep.sh
+++ install_dep.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env bash
 
-version="v0.3.2"
-os="darwin"
-arch="amd64"
-url="https://github.com/golang/dep/releases/download/${version}/dep-${os}-${arch}"
-sha256="39410a604a6cdb16206dec4cf3eb6e30cd653321c68c7325a939321ad0cdc91a"
+set os
+case $OSTYPE in
+    darwin*) os="darwin" ;;
+    linux*) os="linux" ;;
+    *) echo "ERROR unsuported OS $OSTYPE" >&2 ; exit 1 ;;
+esac
 
-check_sha256() {
-    local sum=$1; shift
-    local path=$1; shift
-    local got="$(openssl dgst -sha256 $path | cut -d ' ' -f 2)"
-    if [[ "${got}" != "${sum}" ]]; then
-        echo "ERROR bad checksum" >&2
-        echo "        got:  ${got}" >&2
-        echo "        want: ${sum}" >&2
-        exit 1
-    fi
-}
+version="v0.4.1"
+arch="amd64"
+
+url="https://github.com/golang/dep/releases/download/${version}/dep-${os}-${arch}"
 
 if [[ "$(uname)" != "Darwin" ]]; then
     echo "ERROR currently works only on OSX" >&2
@@ -39,7 +33,6 @@ rm -rf /tmp/install_dep
 mkdir -p /tmp/install_dep
 
 curl -fsSL ${url} -o /tmp/install_dep/dep
-check_sha256 ${sha256} /tmp/install_dep/dep 
 
 chmod +x /tmp/install_dep/dep
 mv /tmp/install_dep/dep /usr/local/bin/dep
