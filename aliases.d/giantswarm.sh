adbef4c364aa53a3976d85da67992552799925e1
Paweł Kopiczko <kopiczko@gmail.com>
2018-04-11 09:53:56 +0200

    aliases.d: update giantswarm aliases

--- aliases.d/giantswarm.sh
+++ aliases.d/giantswarm.sh
@@ -1,23 +1,32 @@
 #!/usr/bin/env bash
 
+alias deployer='GITHUB_TOKEN=$(cat $GOPATH/src/keybase.io/kopiczko/dotfiles/token/github) deployer'
+alias opsctl='OPSCTL_GITHUB_TOKEN=$(cat $GOPATH/src/keybase.io/kopiczko/dotfiles/token/github) opsctl'
+
 onduty() {
-    local token='/keybase/private/kopiczko/token/slack'
+    local token="$GOPATH/src/keybase.io/kopiczko/dotfiles/token/slack"
     if [[ ! -f $token ]]; then
         echo "ERROR file \"$token\" does not exist" >&2
         return 1
     fi
 
-    docker run --rm -ti -e SLACK_TOKEN=$(cat /keybase/private/kopiczko/token/slack) giantswarm/slackutil join --include "support.*" 
-    docker run --rm -ti -e SLACK_TOKEN=$(cat /keybase/private/kopiczko/token/slack) giantswarm/slackutil join --include "project.*"
-    docker run --rm -ti -e SLACK_TOKEN=$(cat /keybase/private/kopiczko/token/slack) giantswarm/slackutil unmute --include "support.*" 
-    docker run --rm -ti -e SLACK_TOKEN=$(cat /keybase/private/kopiczko/token/slack) giantswarm/slackutil unmute --include "project.*"
-    docker run --rm -ti -e SLACK_TOKEN=$(cat /keybase/private/kopiczko/token/slack) giantswarm/slackutil star --include "support.*" 
+    docker run --rm -ti -e SLACK_TOKEN=$(cat $GOPATH/src/keybase.io/kopiczko/dotfiles/token/slack) giantswarm/slackutil join --include "support.*"
+    docker run --rm -ti -e SLACK_TOKEN=$(cat $GOPATH/src/keybase.io/kopiczko/dotfiles/token/slack) giantswarm/slackutil join --include "project.*"
+    docker run --rm -ti -e SLACK_TOKEN=$(cat $GOPATH/src/keybase.io/kopiczko/dotfiles/token/slack) giantswarm/slackutil unmute --include "support.*"
+    docker run --rm -ti -e SLACK_TOKEN=$(cat $GOPATH/src/keybase.io/kopiczko/dotfiles/token/slack) giantswarm/slackutil unmute --include "project.*"
+    #docker run --rm -ti -e SLACK_TOKEN=$(cat $GOPATH/src/keybase.io/kopiczko/dotfiles/token/slack) giantswarm/slackutil star --include "support.*"
 }
 
 dutyend() {
-    docker run --rm -ti -e SLACK_TOKEN=$(cat /keybase/private/kopiczko/token/slack) giantswarm/slackutil unstar --include "support.*" 
-    docker run --rm -ti -e SLACK_TOKEN=$(cat /keybase/private/kopiczko/token/slack) giantswarm/slackutil mute --include "support.*" 
-    docker run --rm -ti -e SLACK_TOKEN=$(cat /keybase/private/kopiczko/token/slack) giantswarm/slackutil mute --include "project.*"
+    local token="$GOPATH/src/keybase.io/kopiczko/dotfiles/token/slack"
+    if [[ ! -f $token ]]; then
+        echo "ERROR file \"$token\" does not exist" >&2
+        return 1
+    fi
+
+    docker run --rm -ti -e SLACK_TOKEN=$(cat $GOPATH/src/keybase.io/kopiczko/dotfiles/token/slack) giantswarm/slackutil unstar --include "support.*"
+    docker run --rm -ti -e SLACK_TOKEN=$(cat $GOPATH/src/keybase.io/kopiczko/dotfiles/token/slack) giantswarm/slackutil mute --include "support.*"
+    docker run --rm -ti -e SLACK_TOKEN=$(cat $GOPATH/src/keybase.io/kopiczko/dotfiles/token/slack) giantswarm/slackutil mute --include "project.*"
 } 
 
 vault-decrypt() {
