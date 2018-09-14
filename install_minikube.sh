7f926f4e8af6c8dd5b01d1bc4293bd891053f2e3
Paweł Kopiczko <kopiczko@gmail.com>
2018-09-14 11:13:52 +0100

    install_minikube.sh: bump to v0.28.2

--- install_minikube.sh
+++ install_minikube.sh
@@ -11,7 +11,7 @@ rm -rf /tmp/install_minikube
 mkdir -p /tmp/install_minikube
 
 #version=latest
-version=v0.25.2
+version=v0.28.2
 
 curl -Lo /tmp/install_minikube/minikube https://storage.googleapis.com/minikube/releases/${version}/minikube-darwin-amd64
 chmod +x /tmp/install_minikube/minikube
